58edb498b19acd691f4bd070ac359141eb854e70
hemae22 <hema.elangovan90@gmail.com>
2017-12-19 17:39:41 +0530

    Config File Settings

---
+++ databaseconfig.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#import preprocessing
+#Configuration File
+
+mypsql = {'host': 'localhost',
+         'port': '5432',
+         'user': 'postgres',
+         'passwd': 'hema',
+         'db': 'carcomponent_shopping'}
+#preprocessing_queue = [preprocessing.scale_and_center,
+#                       preprocessing.dot_reduction,
+#                       preprocessing.connect_lines]
+#use_anonymous = True
